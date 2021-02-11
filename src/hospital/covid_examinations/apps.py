cc581c7ecfa8b69c86a6d12a19dc411ede42b871
Ido Azulay <idoazzz@gmail.com>
2021-02-11 20:21:33 +0200

    .

--- src/hospital/covid_examinations/apps.py
+++ src/hospital/covid_examinations/apps.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
-APP_NAME = 'covid_examinations'
-
 
 class CovidExaminationsConfig(AppConfig):
-    name = APP_NAME
+    name = 'covid_examinations'
